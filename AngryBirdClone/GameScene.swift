86add62c3d17cad53efb1ddae7008719847ab215
Syncopa Productions <info@syncopa.net>
2021-08-17 16:56:25 +0300

    Initial commit

---
+++ AngryBirdClone/GameScene.swift
@@ -0,0 +1,45 @@
+//
+//  GameScene.swift
+//  AngryBirdClone
+//
+//  Created by Rafi Saar on 17/08/2021.
+//  Copyright © 2021 Syncopa Productions. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+           }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
